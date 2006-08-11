3c4d14a4589f7d4860cd8d0bd140a25a4d3586c5
Harri Pitkänen <hatapitk@iki.fi>
2006-08-11 14:04:26 +0000

    * Make word.py fully translatable
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/joukahainen@291 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- wwwroot/word.py
+++ wwwroot/word.py
@@ -33,13 +33,13 @@ _ = _apply_config.translation.ugettext
 
 def edit(req, wid = None):
 	if (wid == None):
-		joheaders.error_page(req, _(u'Parameter wid is required'))
+		joheaders.error_page(req, _(u'Parameter %s is required') % u'wid')
 		return '\n'
 	wid_n = jotools.toint(wid)
 	db = jodb.connect()
 	results = db.query("select word, class from word where wid = %i" % wid_n)
 	if results.ntuples() == 0:
-		joheaders.error_page(req, u'Sanaa %i ei ole\n' % wid_n)
+		joheaders.error_page(req, _(u'Word %i does not exist') % wid_n)
 		return '\n'
 	wordinfo = results.getresult()[0]
 	(uid, uname, editable) = jotools.get_login_user(req)
@@ -51,14 +51,14 @@ def edit(req, wid = None):
 
 def change(req, wid = None):
 	if req.method != 'POST':
-		joheaders.error_page(req, u'Vain POST-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only POST requests are allowed'))
 		return '\n'
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Ei oikeuksia tietojen muuttamiseen')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	if (wid == None):
-		joheaders.error_page(req, u'Parametri wid on pakollinen')
+		joheaders.error_page(req, _(u'Parameter %s is required') % u'wid')
 		return '\n'
 	
 	wid_n = jotools.toint(wid)
@@ -66,7 +66,7 @@ def change(req, wid = None):
 	db.query("begin")
 	wclass_results = db.query("select class from word where wid = %i" % wid_n)
 	if wclass_results.ntuples() == 0:
-		joheaders.error_page(req, u'Sanaa %i ei ole\n' % wid_n)
+		joheaders.error_page(req, _(u'Word %i does not exist') % wid_n)
 		db.query("rollback")
 		return '\n'
 	wclass = wclass_results.getresult()[0][0]
@@ -124,16 +124,16 @@ def change(req, wid = None):
 def flags(req, wid = None):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Muokkausoikeus puuttuu')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	if wid == None:
-		joheaders.error_page(req, u'Parametri wid on pakollinen')
+		joheaders.error_page(req, _(u'Parameter %s is required') % u'wid')
 		return '\n'
 	wid_n = jotools.toint(wid)
 	db = jodb.connect()
 	results = db.query("select word, class from word where wid = %i" % wid_n)
 	if results.ntuples() == 0:
-		joheaders.error_page(req, u'Sanaa %i ei ole\n' % wid_n)
+		joheaders.error_page(req, _(u'Word %i does not exist') % wid_n)
 		return '\n'
 	wordinfo = results.getresult()[0]
 	if req.method == 'GET': # show editor
@@ -145,7 +145,7 @@ def flags(req, wid = None):
 		joheaders.page_footer(req)
 		return "</html>"
 	if req.method != 'POST':
-		joheaders.error_page(req, u'Vain GET/POST-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only GET and POST requests are allowed'))
 		return '\n'
 	db.query("begin")
 	edfield_results = db.query(("SELECT a.aid, a.descr, CASE WHEN fav.wid IS NULL THEN 'f' ELSE 't' END " +
@@ -173,11 +173,11 @@ def flags(req, wid = None):
 		if newval == False:
 			db.query(("delete from flag_attribute_value where wid = %i " +
 			          "and aid = %i") % (wid_n, attribute[0]))
-			messages.append(u"Lippu poistettu: '%s'" % unicode(attribute[1], 'UTF-8'))
+			messages.append(_(u"Flag removed: '%s'") % unicode(attribute[1], 'UTF-8'))
 		if newval == True:
 			db.query(("insert into flag_attribute_value(wid, aid, eevent) " +
 			          "values(%i, %i, %i)") % (wid_n, attribute[0], eid))
-			messages.append(u"Lippu lisätty: '%s'" % unicode(attribute[1], 'UTF-8'))
+			messages.append(_(u"Flag added: '%s'") % unicode(attribute[1], 'UTF-8'))
 	
 	comment = jotools.get_param(req, 'comment', u'')
 	
@@ -198,16 +198,16 @@ def flags(req, wid = None):
 def rwords(req, wid = None):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Muokkausoikeus puuttuu')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	if wid == None:
-		joheaders.error_page(req, u'Parametri wid on pakollinen')
+		joheaders.error_page(req, _(u'Parameter %s is required') % u'wid')
 		return '\n'
 	wid_n = jotools.toint(wid)
 	db = jodb.connect()
 	results = db.query("select word, class from word where wid = %i" % wid_n)
 	if results.ntuples() == 0:
-		joheaders.error_page(req, u'Sanaa %i ei ole\n' % wid_n)
+		joheaders.error_page(req, _(u'Word %i does not exist') % wid_n)
 		return '\n'
 	wordinfo = results.getresult()[0]
 	if req.method == 'GET': # show editor
@@ -219,7 +219,7 @@ def rwords(req, wid = None):
 		joheaders.page_footer(req)
 		return "</html>"
 	if req.method != 'POST':
-		joheaders.error_page(req, u'Vain GET/POST-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only GET and POST requests are allowed'))
 		return '\n'
 	db.query("begin")
 	rword_results = db.query("SELECT rwid, related_word FROM related_word WHERE wid = %i" % wid_n)
@@ -240,7 +240,8 @@ def rwords(req, wid = None):
 			event_inserted = True
 		db.query("delete from related_word where wid = %i and rwid = %i" \
 		         % (wid_n, attribute[0]))
-		messages.append(u"Kirjoitusasu poistettu: '%s'" % unicode(attribute[1], 'UTF-8'))
+		messages.append(_(u"Alternative spelling removed: '%s'") \
+		                % jotools.escape_html(unicode(attribute[1], 'UTF-8')))
 	
 	newwords = jotools.get_param(req, 'add', u'')
 	for word in jotools.unique(newwords.split()):
@@ -257,7 +258,7 @@ def rwords(req, wid = None):
 			event_inserted = True
 		db.query("insert into related_word(wid, eevent, related_word) values(%i, %i, '%s')" \
 		         % (wid_n, eid, jotools.escape_sql_string(word)))
-		messages.append(u"Kirjoitusasu lisätty: '%s'" % word)
+		messages.append(_(u"Alternative spelling added: '%s'") % jotools.escape_html(word))
 	
 	comment = jotools.get_param(req, 'comment', u'')
 	
@@ -288,7 +289,7 @@ def _get_class_selector(classlist, cid, row, include_no_class):
 			         % (row, res[0], jotools.escape_html(unicode(res[1], 'UTF-8')))
 		if include_no_class:
 			retstr = retstr + (u'<label><input type="radio" name="class%i" ' +
-			                     'value="0">virheellinen sana</input></label>') % row
+			    'value="0">%s</input></label>') % (row, _(u'incorrect word'))
 		return retstr
 	else:
 		for res in classlist:
@@ -353,7 +354,7 @@ def _list_homonyms(db, word):
 		    % (r[0], jotools.escape_html(unicode(r[1], 'UTF-8')),
 		       jotools.escape_html(unicode(r[2], 'UTF-8')))
 	if len(h) == 0: return u''
-	else: return u'Homonyymit: ' + h
+	else: return _(u'Homonyms') + u': ' + h
 
 # Stores a new word into the database and marks the original in raw_words as processed
 # (if needed). Returns (wid, add_entry_structure) where wid is the identifier for
@@ -367,12 +368,12 @@ def _store_word(db, word, uid):
 	          'wid': None, 'error': None}
 	if not jotools.checkword(word['word']):
 		retval['wid'] = None
-		retval['error'] = u'Sanassa on kiellettyjä merkkejä'
+		retval['error'] = _(u'Forbidden characters in the word')
 		retval['try_again'] = False
 		return retval
 	if word['cid'] == None:
 		retval['wid'] = None
-		retval['error'] = u'Sanaluokka on asettamatta'
+		retval['error'] = _(u'Word class must be set')
 		retval['try_again'] = True
 		return retval
 	if word['cid'] != 0:
@@ -387,23 +388,23 @@ def _store_word(db, word, uid):
 		db.query("INSERT INTO word(wid, word, class, cuser) VALUES(%i, '%s', %i, %i)" \
 		         % (retval['wid'], jotools.escape_sql_string(retval['word']),
 		            retval['cid'], uid))
-		retval['error'] = u'<a href="edit?wid=%i">Sana lisätty</a>' % retval['wid']
+		retval['error'] = u'<a href="edit?wid=%i">%s</a>' % (retval['wid'], _(u'Word added'))
 	if word['oword'] != None:
 		db.query("UPDATE raw_word SET processed = TRUE WHERE word = '%s'" \
 		         % jotools.escape_sql_string(word['oword']))
 		if retval['error'] == None:
-			retval['error'] = u'Sana poistettu ehdotettujen sanojen listalta'
+			retval['error'] = _(u'Word removed from the list of candidate words')
 	retval['try_again'] = False
-	if retval['error'] == None: retval['error'] = u'Ei tehty mitään'
+	if retval['error'] == None: retval['error'] = _(u'Nothing was done')
 	return retval
 
 def add_from_db(req):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Ei oikeuksia tietojen muuttamiseen')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	if req.method != 'GET':
-		joheaders.error_page(req, u'Vain GET-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only GET requests are allowed'))
 		return '\n'
 	db = jodb.connect()
 	words_per_page = 15
@@ -421,18 +422,18 @@ def add_from_db(req):
 	                    "WHERE processed = FALSE %s " +
 	                    "ORDER BY word %s") % (condition, limit))
 	if results.ntuples() == 0 and category == None:
-		joheaders.error_page(req, u'Tietokannassa ei ole lisäystä odottavia ' +
-		          u'sanoja.')
+		joheaders.error_page(req, _(u'There are no words to be added'))
 		return '\n'
 	if results.ntuples() == 0 and category != None:
-		joheaders.error_page(req, u'Tietokannassa ei ole lisäystä odottavia ' +
-		          u'sanoja kategoriassa %s.' % jotools.escape_html(category))
+		joheaders.error_page(req, _(u'There are no words to be added') + u' ' +
+		          _(u'in category %s') % jotools.escape_html(category))
 		return '\n'
 	class_res = db.query("select classid, name from wordclass").getresult()
-	joheaders.list_page_header(req, u"Joukahainen &gt; Lisää sanoja", uid, uname)
+	joheaders.list_page_header(req, u"Joukahainen &gt; " + _(u"Add words"), uid, uname)
 	jotools.write(req, u'<form method="post" action="add">\n')
 	jotools.write(req, u'<table class="border">\n')
-	jotools.write(req, u'<tr><th>Sana</th><th>Sanaluokka</th><th>Huomioita</th></tr>\n')
+	jotools.write(req, u'<tr><th>%s</th><th>%s</th><th>%s</th></tr>\n' \
+	                   % (_(u'Word'), _(u'Word class'), _(u'Notes')))
 	i = 0
 	for result in results.getresult():
 		word = unicode(result[0], 'UTF-8')
@@ -447,37 +448,38 @@ def add_from_db(req):
 		jotools.write(req, u'</td></tr>\n')
 		i = i + 1
 	jotools.write(req, u'</table>\n' +
-	                   u'<p><input type="submit" value="Lisää sanoja"></p></form>\n')
+	                   u'<p><input type="submit" value="%s"></p></form>\n' % _(u"Add words"))
 	joheaders.list_page_footer(req)
 	return '</html>\n'
 
 def add_manual(req):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Ei oikeuksia tietojen muuttamiseen')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	if req.method != 'GET':
-		joheaders.error_page(req, u'Vain GET-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only GET requests are allowed'))
 		return '\n'
 	db = jodb.connect()
 	words_per_page = 15
-	joheaders.list_page_header(req, u"Joukahainen &gt; Lisää sanoja", uid, uname)
+	joheaders.list_page_header(req, u"Joukahainen &gt; " + _(u"Add words"), uid, uname)
 	jotools.write(req, u'<form method="post" action="add">\n' +
-	                   u'<table class="border">\n<tr><th>Sana</th><th>Sanaluokka</th></tr>\n')
+	                   u'<table class="border">\n<tr><th>%s</th><th>%s</th></tr>\n' \
+	                   % (_(u'Word'), _(u'Word class')))
 	_add_entry_fields(req, db, None, words_per_page)
 	jotools.write(req, u'</table>\n' +
-	                   u'<p><input type="submit" value="Lisää sanoja"></p></form>\n')
+	                   u'<p><input type="submit" value="%s"></p></form>\n' % _(u"Add words"))
 	joheaders.list_page_footer(req)
 	return '</html>\n'
 
 def add(req):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Ei oikeuksia tietojen muuttamiseen')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	db = jodb.connect()
 	if req.method != 'POST':
-		joheaders.error_page(req, u'Vain POST-pyynnöt ovat sallittuja')
+		joheaders.error_page(req, _(u'Only POST requests are allowed'))
 		return '\n'
 	db.query("BEGIN")
 	if jotools.get_param(req, 'confirm', u'') == u'on': confirm = True
@@ -497,7 +499,7 @@ def add(req):
 		else: nclass = None
 		word['cid'] = nclass
 		if confirm and nclass != 0 and jotools.get_param(req, 'confirm%i' % i, u'') != u'on':
-			word['error'] = u'Sanaa ei lisätty'
+			word['error'] = _(u'Word was not added')
 			word['try_again'] = False
 		if jotools.get_param(req, 'confirm%i' % i, u'') == u'on': word['confirmed'] = True
 		stored_word = _store_word(db, word, uid)
@@ -505,34 +507,36 @@ def add(req):
 		if stored_word['try_again']: need_confirm_count = need_confirm_count + 1
 		nwordlist.append(stored_word)
 	db.query("COMMIT")
-	joheaders.list_page_header(req, u'Joukahainen &gt; Lisää sanoja', uid, uname)
+	joheaders.list_page_header(req, u"Joukahainen &gt; " + _(u"Add words"), uid, uname)
 	if need_confirm_count > 0:
-		jotools.write(req, u'<p>Joidenkin sanojen lisäys ei onnistunut tai vaatii ' +
-		              u'vahvistuksen. Tee tarvittavat korjaukset ja merkitse rastilla ' +
-			    u'sanat, jotka edelleen haluat lisättäviksi.</p>')
+		jotools.write(req, u'<p>' + _(u'''Adding some words failed or requires confirmation.
+Make the required changes and mark the words that you still want to add.''') + u'</p>')
 		jotools.write(req, u'<form method="post" action="add">\n')
-		jotools.write(req, u'<table class="border"><tr><th>Sana</th><th>Sanaluokka</th>' +
-		              u'<th>Vahvista sanan lisäys</th><th>Huomioita</th></tr>\n')
+		jotools.write(req,
+		  u'<table class="border"><tr><th>%s</th><th>%s</th><th>%s</th><th>%s</th></tr>\n' \
+		  % (_(u'Word'), _(u'Word class'), _(u'Confirm addition'), _(u'Notes')))
 		_add_entry_fields(req, db, nwordlist, None)
 		jotools.write(req, u'</table>\n<p>' +
 		                   u'<input type="hidden" name="confirm" value="on">' +
-		                   u'<input type="submit" value="Jatka"></p></form>\n')
+		                   u'<input type="submit" value="%s"></p></form>\n' % _(u'Continue'))
 		joheaders.list_page_footer(req)
 		return '</html>\n'
 	else:
-		jotools.write(req, u'<p>Seuraavat muutokset tehtiin:</p>')
-		jotools.write(req, u'<table class="border"><tr><th>Sana</th><th>Sanaluokka</th>' +
-		              u'<th>Huomioita</th></tr>\n')
+		jotools.write(req, u'<p>%s:</p>' % _(u'The following changes were made'))
+		jotools.write(req,
+		  u'<table class="border"><tr><th>%s</th><th>%s</th><th></th></tr>\n' \
+		  % (_(u'Word'), _(u'Word class'), _(u'Notes')))
 		_add_entry_fields(req, db, nwordlist, None)
 		jotools.write(req, u'</table>\n')
-		jotools.write(req, u'<p><a href="../">Takaisin aloitussivulle ...</a></p>\n')
+		jotools.write(req, u'<p><a href="../">%s ...</a></p>\n' \
+		                   % _(u'Back to main page'))
 		joheaders.list_page_footer(req)
 		return '</html>\n'
 
 def categories(req):
 	(uid, uname, editable) = jotools.get_login_user(req)
 	if not editable:
-		joheaders.error_page(req, u'Ei oikeuksia tietojen muuttamiseen')
+		joheaders.error_page(req, _(u'You are not allowed to edit data'))
 		return '\n'
 	db = jodb.connect()
 	results = db.query("SELECT coalesce(info, ''), count(*) FROM raw_word " +
@@ -540,19 +544,21 @@ def categories(req):
 	                   "GROUP BY coalesce(info, '') " +
 	                   "ORDER BY coalesce(info, '') ")
 	if results.ntuples() == 0:
-		joheaders.error_page(req, u'Tietokannassa ei ole lisäystä odottavia sanoja.')
+		joheaders.error_page(req, _(u'There are no words to be added'))
 		return '\n'
-	joheaders.list_page_header(req, u"Joukahainen &gt; Lisää sanoja", uid, uname)
-	jotools.write(req, u"<p>Valitse kategoria, jonka sanoja haluat lisätä:</p>\n")
-	jotools.write(req, u"<table><tr><th>Kategoria</th><th>Sanoja jäljellä</th></tr>\n")
+	joheaders.list_page_header(req, u"Joukahainen &gt; " + _(u"Add words"), uid, uname)
+	jotools.write(req, u"<p>%s:</p>\n" \
+	                   % _(u'Choose a category from which you want to add words'))
+	jotools.write(req, u"<table><tr><th>%s</th><th>%s</th></tr>\n" \
+	                   % (_(u'Category'), _(u'Words left')))
 	for result in results.getresult():
 		cat = unicode(result[0], 'UTF-8')
-		if cat == u'': cats = u'(ei kategoriaa)'
+		if cat == u'': cats = u'(' + _(u'no category') + u')'
 		else: cats = cat
 		jotools.write(req, (u'<tr><td><a href="add_from_db?category=%s">%s</a></td>' +
 		                    u'<td>%i</td></tr>\n') \
 		  % (jotools.escape_url(cat), jotools.escape_html(cats), result[1]))
 	jotools.write(req, u"</table>\n")
-	jotools.write(req, u'<p><a href="add_from_db">Kaikki sanat ...</a></p>\n')
+	jotools.write(req, u'<p><a href="add_from_db">%s ...</a></p>\n' % _(u'All words'))
 	joheaders.list_page_footer(req)
 	return '</html>\n'
