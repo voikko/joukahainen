8956e963121e59ab50f408609ad22419682129e5
Harri Pitkänen <hatapitk@iki.fi>
2006-08-24 10:35:11 +0000

    * Add support for searching by text attribute value
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/joukahainen@396 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- wwwroot/query.py
+++ wwwroot/query.py
@@ -31,11 +31,18 @@ import jodb
 _ = _apply_config.translation.ugettext
 
 def form(req):
+	db = jodb.connect()
 	joheaders.page_header(req, _(u'Search database'))
 	jotools.write(req, u'<form method="get" action="wlist">\n<p>')
 	jotools.write(req, u'<label>%s: <input type="text" name="word" /></label>\n' % _(u'Word'))
 	jotools.write(req, u'<label>%s <input type="checkbox" name="wordre" /></label></p><p>\n' \
 	              % _(u'Use regular expression'))
+	textattrs = db.query("SELECT aid, descr FROM attribute WHERE type = 1").getresult()
+	jotools.write(req, u'<select name="textaid">\n')
+	jotools.write(req, u'<option selected="1" value="">(%s)</option>\n' % _(u'select attribute'))
+	for (aid, dsc) in textattrs:
+		jotools.write(req, u'<option value="%i">%s</option>\n' % (aid, unicode(dsc, 'UTF-8')))
+	jotools.write(req, u'</select> %s <input type="text" name="textvalue" /><br />\n' % _(u'is'))
 	for (tname, tdesc) in jooutput.list_supported_types():
 		if tname == 'html': selected = u'checked="checked"'
 		else: selected = u''
@@ -58,6 +65,15 @@ def wlist(req):
 		if jotools.get_param(req, 'wordre', u'') == u'on': compop = 'SIMILAR TO'
 		else: compop = '='
 		conditions.append("w.word %s '%s'" % (compop, jotools.escape_sql_string(word)))
+	aid = jotools.toint(jotools.get_param(req, 'textaid', u''))
+	if aid != 0:
+		value = jotools.get_param(req, 'textvalue', u'')
+		if value == u'':
+			cond = "w.wid NOT IN (SELECT wid FROM string_attribute_value WHERE aid = %i)" % aid
+		else:
+			cond = ("w.wid IN (SELECT wid FROM string_attribute_value " +
+			        "WHERE aid = %i AND value = '%s')") % (aid, jotools.escape_sql_string(value))
+		conditions.append(cond)
 	# FIXME: user should be able to select the order
 	order = "ORDER BY w.word, c.name, w.wid"
 	# Build the full select clause
