48d029b15677b256d5bdad4b4560c3bd58364653
Harri Pitkänen <hatapitk@iki.fi>
2006-10-04 15:43:30 +0000

    * Add tool to list all changes between two dates
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/joukahainen@587 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- wwwroot/query.py
+++ wwwroot/query.py
@@ -22,6 +22,8 @@
 from mod_python import apache
 
 import sys
+import datetime
+import time
 import _apply_config
 import joheaders
 import jotools
@@ -103,3 +105,71 @@ def wlist(req):
 	outputtype = jotools.get_param(req, "listtype", u'html')
 	jooutput.call(req, outputtype, select)
 	return "\n"
+
+def listchanges(req, sdate = None, edate = None):
+	db = jodb.connect()
+	(uid, uname, editable) = jotools.get_login_user(req)
+	joheaders.page_header_navbar_level1(req, _(u'List changes'), uid, uname)
+	
+	edt = datetime.datetime.now()
+	sdt = edt - datetime.timedelta(days=1)
+	if sdate != None:
+		try:
+			stime = time.strptime(sdate, u'%Y-%m-%d')
+			sdt = datetime.datetime(*stime[0:5])
+		except:
+			jotools.write(req, "<p>%s</p>\n" % _("Invalid start date"))
+	if edate != None:
+		try:
+			etime = time.strptime(edate, u'%Y-%m-%d')
+			edt = datetime.datetime(*etime[0:5])
+		except:
+			jotools.write(req, "<p>%s</p>\n" % _("Invalid end date"))
+	sdate_s = sdt.strftime('%Y-%m-%d')
+	edate_s = edt.strftime('%Y-%m-%d')
+	
+	jotools.write(req, u"""
+<form method="get" action="listchanges">
+<label>%s <input type="text" name="sdate" value="%s"/></label><br />
+<label>%s <input type="text" name="edate" value="%s"/></label><br />
+<input type="submit" /> <input type="reset" />
+</form>
+	""" % (_(u'Start date'), sdate_s, _(u'End date'), edate_s))
+	
+	
+	results = db.query("""
+	SELECT u.uname, to_char(w.ctime, 'YYYY-MM-DD HH24:MI:SS'),
+	       coalesce(u.firstname, ''), coalesce(u.lastname, ''),
+	       '%s', NULL, w.wid, w.word
+	FROM word w, appuser u WHERE w.cuser = u.uid AND w.ctime > '%s'
+	UNION
+	SELECT u.uname, to_char(e.etime, 'YYYY-MM-DD HH24:MI:SS'),
+	       coalesce(u.firstname, ''), coalesce(u.lastname, ''),
+	       e.message, e.comment, w.wid, w.word
+	FROM appuser u, event e, word w WHERE u.uid = e.euser AND e.eword = w.wid
+	AND e.etime > '%s'
+	ORDER BY 2 DESC""" % (_(u'Word created'), sdate_s, sdate_s));
+	
+	retstr = u''
+	for result in results.getresult():
+		wordlink = u'<a href="../word/edit?wid=%i">%s</a>' \
+		           % (result[6], jotools.escape_html(unicode(result[7], 'UTF-8')))
+		date = result[1]
+		user = jotools.escape_html(unicode(result[2], 'UTF-8')) + u" " + \
+		       jotools.escape_html(unicode(result[3], 'UTF-8')) + u" (" + \
+		       jotools.escape_html(unicode(result[0], 'UTF-8')) + u")"
+		retstr = retstr + (u'<div class="logitem"><p class="date">%s %s %s</p>\n' \
+		                   % (wordlink, user, date))
+		if result[4] != None:
+			msg = jotools.escape_html(unicode(result[4], 'UTF-8')).strip()
+			msg = msg.replace(u'\n', u'<br />\n')
+			retstr = retstr + u'<p class="logmsg">%s</p>\n' % msg
+		if result[5] != None:
+			comment = jotools.escape_html(unicode(result[5], 'UTF-8')).strip()
+			comment = comment.replace(u'\n', u'<br />\n')
+			retstr = retstr + u'<p class="comment">%s</p>\n' % comment
+		retstr = retstr + u"</div>\n"
+	
+	jotools.write(req, retstr)
+	joheaders.page_footer_plain(req)
+	return '\n'
