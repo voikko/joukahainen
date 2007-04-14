cd8f90ca155272c499d048b2be64a5e5889c0852
Harri Pitkänen <hatapitk@iki.fi>
2007-04-14 13:41:17 +0000

    * Remove code related to Joukahainen -> Malaga export, which   is no longer done
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk/joukahainen@1297 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- langpacks/fi/functions.py
+++ langpacks/fi/functions.py
@@ -32,7 +32,6 @@ import sys
 sys.path.append(MODULE_PATH_HFTOOLS)
 import hfaffix
 import hfutils
-import hfconv
 import voikkoutils
 import xml.sax.saxutils
 import time
@@ -41,46 +40,26 @@ import time
 def _get_db_vowel_type(db, wid):
 	results = db.query(("SELECT aid FROM flag_attribute_value " +
 	                    "WHERE wid = %i AND aid IN (22, 23)") % wid)
-	if results.ntuples() == 0: return hfutils.VOWEL_DEFAULT
+	if results.ntuples() == 0: return voikkoutils.VOWEL_DEFAULT
 	elif results.ntuples() == 1:
-		if results.getresult()[0][0] == 22: return hfutils.VOWEL_FRONT
-		else: return hfutils.VOWEL_BACK
-	else: return hfutils.VOWEL_BOTH
-
-
-# Returns autodetected vowel type of infection suffixes for a simple word.
-# If word contains character '=', automatic detection is only performed on the
-# trailing part. If word contains character '|', automatic detection is performed
-# on the trailing part and the whole word, and the union of accepted vowel types is returned.
-def _get_wordform_infl_vowel_type(wordform):
-	# Search for last '=' or '-', check the trailing part using recursion
-	startind = max(wordform.rfind(u'='), wordform.rfind(u'-'))
-	if startind == len(wordform) - 1: return hfutils.VOWEL_BOTH # Not allowed
-	if startind != -1: return _get_wordform_infl_vowel_type(wordform[startind+1:])
-	
-	# Search for first '|', check the trailing part using recursion
-	startind = wordform.find(u'|')
-	if startind == len(wordform) - 1: return hfutils.VOWEL_BOTH # Not allowed
-	vtype_whole = hfutils.vowel_type(wordform)
-	if startind == -1: return vtype_whole
-	vtype_part = _get_wordform_infl_vowel_type(wordform[startind+1:])
-	if vtype_whole == vtype_part: return vtype_whole
-	else: return hfutils.VOWEL_BOTH
+		if results.getresult()[0][0] == 22: return voikkoutils.VOWEL_FRONT
+		else: return voikkoutils.VOWEL_BACK
+	else: return voikkoutils.VOWEL_BOTH
 
 
 # Returns the vowel type of inflection suffixes for a word in the database.
 def _get_infl_vowel_type(db, wid, word):
 	# Check the database
 	dbtype = _get_db_vowel_type(db, wid)
-	if dbtype != hfutils.VOWEL_DEFAULT: return dbtype
+	if dbtype != voikkoutils.VOWEL_DEFAULT: return dbtype
 	# Check alternative spellings
 	altforms_res = db.query('SELECT related_word FROM related_word WHERE wid = %i' % wid)
 	for altform_r in altforms_res.getresult():
 		altform = unicode(altform_r[0], 'UTF-8')
 		if altform.replace(u'|', u'').replace(u'=', u'') == word:
-			return _get_wordform_infl_vowel_type(altform)
+			return voikkoutils.get_wordform_infl_vowel_type(altform)
 	# Return the default
-	return _get_wordform_infl_vowel_type(word)
+	return voikkoutils.get_wordform_infl_vowel_type(word)
 
 
 WCHARS = u"abcdefghijklmnopqrstuvwxyzåäöszèéšžáóABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖŠŽÈÉŠŽ-'|="
@@ -193,161 +172,6 @@ def word_inflection(db, wid, word, classid):
 	if note == u'': notetext = u''
 	else: notetext = u'<p>%s</p>\n' % note
 	return notetext + table_header + retdata + u'</table>\n'
-
-# Returns tuple (alku, jatko) for given word in Joukahainen
-def _find_malaga_word_class(word, hf_class, hf_histclass, hf_wordclass):
-	# If historical class was set, see first if it can be used in Malaga
-	for histclass in hfconv.histmap:
-		if hf_histclass == histclass[0]:
-			for infclass in hfconv.classmap:
-				for subclass in infclass[2]:
-					if subclass[2] == histclass[1]:
-						alku = hfconv.match_re(word, subclass[1])
-						if alku != None: return (alku, hf_histclass)
-	
-	# No (usable) historical class, use the modern class
-	classparts = hf_class.split(u'-')
-	if len(classparts) == 1:
-		classparts.append(None)
-		gradtypes = [ None ]
-	else:
-		gradtypes = []
-		for grad in hfconv.grads:
-			if grad[2] == classparts[1]: gradtypes.append(grad[1])
-	
-	for infclass in hfconv.classmap:
-		if infclass[0] != classparts[0]: continue
-		for subclass in infclass[2]:
-			if len(subclass) > 3 and not hf_wordclass in subclass[3]: continue
-			if not subclass[0] in gradtypes: continue
-			alku = hfconv.match_re(word, subclass[1])
-			if alku != None: return (alku, subclass[2])
-	
-	return (None, None)
-
-# Returns tuple (luokka, tiedot) for given word in Joukahainen
-def _get_class_and_flags(db, hf_wclass, wid):
-	flags = db.query('SELECT aid FROM flag_attribute_value WHERE wid = %i' % wid)
-	flag_s = u''
-	if hf_wclass == 1: class_s = u'nimisana'
-	elif hf_wclass == 2: class_s = u'laatusana'
-	elif hf_wclass == 3: class_s = u'teonsana'
-	for flag in flags.getresult():
-		if hf_wclass == 2 and flag[0] == 10: class_s = u'nimi_laatusana'
-		elif hf_wclass == 1 and flag[0] == 11: class_s = u'etunimi'
-		elif hf_wclass == 1 and flag[0] == 12: class_s = u'sukunimi'
-		elif hf_wclass == 1 and flag[0] == 13: class_s = u'paikannimi'
-		elif hf_wclass == 1 and flag[0] == 14: class_s = u'nimi'
-		else:
-			for f in hfconv.flagmap:
-				if flag[0] == f[1]: flag_s = flag_s + f[0] + u', '
-	if flag_s == u'': flag_s = None
-	else: flag_s = flag_s[:-2]
-	return (class_s, flag_s)
-
-# Returns a string describing the structure of a word, if necessary for the spellchecker
-# or hyphenator
-def _get_structure(wordform, malaga_class_s):
-	needstructure = False
-	if malaga_class_s in [u'nimi', u'etunimi', u'sukunimi', 'paikannimi']: ispropernoun = True
-	else: ispropernoun = False
-	structstr = u', rakenne: "='
-	for i in range(len(wordform)):
-		c = wordform[i]
-		if c == u'-':
-			structstr = structstr + u"-="
-			needstructure = True
-		elif c == u'|': structstr = structstr
-		elif c == u'=':
-			structstr = structstr + u"="
-			needstructure = True
-		elif c == u':':
-			structstr = structstr + u":"
-			needstructure = True
-		elif c.isupper():
-			structstr = structstr + u"i"
-			if not (ispropernoun and i == 0): needstructure = True
-		else: structstr = structstr + u"p"
-	if needstructure: return structstr + u'"'
-	else: return u""
-
-# Prints a line or lines to be added to Suomi-malaga lexicon for given word
-def _malaga_line(db, req, wid, word, classid, hf_class, hf_histclass):
-	altforms_res = db.query('SELECT related_word FROM related_word WHERE wid = %i' % wid)
-	if altforms_res.ntuples() == 0: altforms = [word]
-	else:
-		altforms = []
-		for res in altforms_res.getresult():
-			altforms.append(unicode(res[0], 'UTF-8'))
-	forced_vtype = _get_db_vowel_type(db, wid)
-	for altform in altforms:
-		word = altform.replace(u'|', u'').replace(u'=', u'')
-		if classid in [1, 2, 3]:
-			(alku, jatko) = _find_malaga_word_class(word, hf_class, hf_histclass, classid)
-			if alku == None:
-				req.write((u"#Malaga class not found for (%s, %i, %s)\n" \
-				              % (word, classid, hf_class)).encode('UTF-8'))
-				continue
-			if forced_vtype == hfutils.VOWEL_DEFAULT:
-				vtype = _get_wordform_infl_vowel_type(altform)
-			else: vtype = forced_vtype
-			if vtype == hfutils.VOWEL_FRONT: malaga_vtype = u'ä'
-			elif vtype == hfutils.VOWEL_BACK: malaga_vtype = u'a'
-			elif vtype == hfutils.VOWEL_BOTH: malaga_vtype = u'aä'
-			(wclass_s, flags) = _get_class_and_flags(db, classid, wid)
-			if flags == None: flags = u''
-			else: flags = u', tiedot: <%s>' % flags
-			req.write((u'[perusmuoto: "%s", alku: "%s", luokka: %s, jatko: <%s>, äs: %s%s%s];\n' \
-			          % (word, alku, wclass_s, jatko, malaga_vtype, flags,
-				   _get_structure(altform, wclass_s))).encode('UTF-8'))
-
-def _jooutput_malaga(req, db, query):
-	req.content_type = "text/plain"
-	req.send_http_header()
-	req.write((u"""# This file is generated for use in Suomi-malaga by
-# vocabulary management application Joukahainen.
-# This data is based on Suomi-malaga 0.7 by Hannu Väisänen, and includes
-# modifications from people listed in file CONTRIBUTORS of current Suomi-malaga
-# Voikko edition Subversion repository (or, if you have received this file as a
-# part of Suomi-malaga, the file is located at the root directory of the source
-# package). For more information, see http://joukahainen.lokalisointi.org
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# Time of generation: %s
-
-""" % time.strftime("%Y-%m-%d %H:%M:%S %Z")).encode('UTF-8'))
-	results = db.query(("SELECT w.word, w.class, sa.value, w.wid, hc.value, fc.value, cf.aid " +
-	"FROM string_attribute_value sa, " +
-	"(%s) w LEFT JOIN string_attribute_value hc ON (w.wid = hc.wid AND hc.aid = 16) " +
-	"LEFT JOIN int_attribute_value fc ON (w.wid = fc.wid AND fc.aid = 38) " +
-	"LEFT JOIN flag_attribute_value cf ON (w.wid = cf.wid AND cf.aid = 39) " +
-	"WHERE w.wid = sa.wid AND sa.aid = 1 AND sa.value != 'poikkeava' " +
-	"ORDER BY w.word, w.class, w.wid") % query)
-	for result in results.getresult():
-		# drop too rare words
-		if result[5] > 9 or (result[5] == 9 and result[6] == 39): continue
-		wid = result[3]
-		word = unicode(result[0], 'UTF-8')
-		classid = result[1]
-		if result[2] == None: continue
-		hf_class = unicode(result[2], 'UTF-8')
-		if result[4] == None: hf_histclass = None
-		else: hf_histclass = unicode(result[4], 'UTF-8')
-		_malaga_line(db, req, wid, word, classid, hf_class, hf_histclass)
-
 
 def _get_xml_flags(flags, flagtype, flaglist):
 	results = []
@@ -529,15 +353,12 @@ def _jooutput_xml(req, db, query):
 # Lists the language specific output types. This is a mandatory function
 def jooutput_list_supported_types():
 	types = []
-	types.append(('malaga', u'Tulosta Suomi-malagan käyttämässä muodossa'))
 	types.append(('xml', u'Tulosta XML-muodossa'))
 	return types
 
 # Language specific list output. This is a mandatory function
 def jooutput_call(req, outputtype, db, query):
-	if outputtype == 'malaga':
-		_jooutput_malaga(req, db, query)
-	elif outputtype == 'xml':
+	if outputtype == 'xml':
 		_jooutput_xml(req, db, query)
 	else:
 		joheaders.error_page(req, _(u'Unsupported output type'))
